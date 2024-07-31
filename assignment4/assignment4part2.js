1482665710729fc03b05de0dc584d550505bb2e7
Rashid2121 <rashid.patel@dcmail.ca>
2024-07-31 17:17:36 -0400

    commit 4
    added to js file

---
+++ assignment4/assignment4part2.js
@@ -0,0 +1,15 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+const imageAlts = {
+  'pic1.jpg': 'Blue human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white flowers',
+  'pic4.jpg': 'Orange hibiscus',
+  'pic5.jpg': 'Butterfly'
+};
